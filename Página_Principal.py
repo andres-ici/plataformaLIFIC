8af3e46b5a91deaca55cfc115adf21eb057aa2a6
Andres C <a.carrasco12@ufromail.cl>
2024-12-03 11:58:51 -0300

    Se implemento visualizador para todos los modulos

--- Página_Principal.py
+++ Página_Principal.py
@@ -10,6 +10,6 @@ Col1, Col2, Col3 = st.columns(3)
 
 #if st.theme() == 'light':
 
-Col2.image('https://i.imgur.com/YMei8p1.png', use_column_width='auto')
+Col2.image('https://i.imgur.com/YMei8p1.png', use_container_width ='auto')
 
 st.title("Pagina principal de la plataforma")
--- pages/Visualizador.py
+++ pages/Visualizador.py
@@ -21,11 +21,18 @@ def generarDF(url_list2):
         df = df.drop(columns=["Porcentaje Justificadas", "Inasistencias justificadas", "Clases Realizadas"])
         df = df[df["Nombre"].notna() & (df["Nombre"].str.strip() != "")]
         df['Modalidad'] = 'Teórico'
-        df['Módulo'] = str(url_list2[i][-2:])
+        
+        try:
+            df['Módulo'] = int(str(url_list2[i][-2:]))
+        except:
+            df['Módulo'] = 'x'
+            
         df = df[['Nombre','Matrícula', 'Modalidad', 'Módulo', 'Porcentaje Asistencia', 'Inasistencias no justificadas']]
+        df['Porcentaje Asistencia'] = df['Porcentaje Asistencia'].str.replace('%', '').astype(int)
 
         dfFinal = df_combined = pd.concat([df, dfFinal], ignore_index=False)
-
+        
+    print(dfFinal)
     return dfFinal
 
 
@@ -46,7 +53,7 @@ client = gspread.authorize(credentials=credentials)
 Col1, Col2, Col3 = st.columns(3)
 
 
-Col2.image('https://i.imgur.com/YMei8p1.png',use_column_width='auto')
+Col2.image('https://i.imgur.com/YMei8p1.png',use_container_width ='auto')
 
 
 
@@ -64,23 +71,59 @@ if op_modalidad == "Teórico":
     match op_asignatura:
         case "A1":
             op_modulo = st.selectbox("Elegir Módulo", ["Todos","1"])
-            url_list = [st.secrets["A1modulo1"]]
-        case "A2":
-            op_modulo = st.selectbox("Elegir Módulo", ["Todos","1","2"])
-            url_list = [st.secrets["A2modulo1"],st.secrets["A2modulo2"]]
-        case "A3":
-            op_modulo = st.selectbox("Elegir Módulo", ["Todos","1"])
-            url_list = [st.secrets["A3modulo1"]]
-        case "A4":
-            op_modulo = st.selectbox("Elegir Módulo", ["Todos","01"])
-            url_list2 = ["TA4M01"]
+            url_list2 = ["TA1M01"]
+            st.text("Modalidad: {}, Asignatura: {}, Modulo: {}".format(op_modalidad,op_asignatura,op_modulo))
             df = generarDF(url_list2)
-
 
             if op_modulo == "Todos":
                 st.write(df)
             else:
-                st.write(df[df["Módulo"] == op_modulo])
+                try:
+                    st.write(df[df["Módulo"] == int(op_modulo)])
+                except:
+                    st.write(df[df["Módulo"] == op_modulo])
+                    
+        case "A2":
+            op_modulo = st.selectbox("Elegir Módulo", ["Todos","1","2"])
+            url_list2 = ["TA2M01", "TA2M02"]
+            st.text("Modalidad: {}, Asignatura: {}, Modulo: {}".format(op_modalidad,op_asignatura,op_modulo))
+            df = generarDF(url_list2)
+
+            if op_modulo == "Todos":
+                st.write(df)
+            else:
+                try:
+                    st.write(df[df["Módulo"] == int(op_modulo)])
+                except:
+                    st.write(df[df["Módulo"] == op_modulo])
+                    
+        case "A3":
+            op_modulo = st.selectbox("Elegir Módulo", ["Todos","1"])
+            url_list2 = ["TA3M01"]
+            st.text("Modalidad: {}, Asignatura: {}, Modulo: {}".format(op_modalidad,op_asignatura,op_modulo))
+            df = generarDF(url_list2)
+
+            if op_modulo == "Todos":
+                st.write(df)
+            else:
+                try:
+                    st.write(df[df["Módulo"] == int(op_modulo)])
+                except:
+                    st.write(df[df["Módulo"] == op_modulo])
+                    
+        case "A4":
+            op_modulo = st.selectbox("Elegir Módulo", ["Todos","01"])
+            url_list2 = ["TA4M01"]
+            st.text("Modalidad: {}, Asignatura: {}, Modulo: {}".format(op_modalidad,op_asignatura,op_modulo))
+            df = generarDF(url_list2)
+
+            if op_modulo == "Todos":
+                st.write(df)
+            else:
+                try:
+                    st.write(df[df["Módulo"] == int(op_modulo)])
+                except:
+                    st.write(df[df["Módulo"] == op_modulo])
 
     
 
@@ -89,14 +132,57 @@ else: #Práctico
         case "A1":
             op_modulo = st.selectbox("Elegir Módulo", ["Todos","2","3"])
             url_list = [st.secrets["PA1M02"],st.secrets["PA1M03"]]
+            
+            url_list2 = ["PA1M02", "PA1M03"]
+            
+            st.text("Modalidad: {}, Asignatura: {}, Modulo: {}".format(op_modalidad,op_asignatura,op_modulo))
+
+            df = generarDF(url_list2)
+
+            if op_modulo == "Todos":
+                st.write(df)
+            else:
+                try:
+                    st.write(df[df["Módulo"] == int(op_modulo)])
+                except:
+                    st.write(df[df["Módulo"] == op_modulo])
+                    
         case "A2":
             op_modulo = st.selectbox("Elegir Módulo", ["Todos","4","5","6","7","8","9","10","11","12","13","14","15","17","18","19","20","21","22","23","24"])
-            url_list = [st.secrets["PA2M04"],st.secrets["PA1M05"],st.secrets["PA1M06"],st.secrets["PA1M07"],st.secrets["PA1M08"],st.secrets["PA1M09"],st.secrets["PA1M10"],st.secrets["PA1M11"],st.secrets["PA1M12"],st.secrets["PA1M13"],st.secrets["PA1M14"],st.secrets["PA1M15"],st.secrets["PA1M17"],st.secrets["PA1M18"],st.secrets["PA1M19"],st.secrets["PA1M20"],st.secrets["PA1M21"],st.secrets["PA1M22"],st.secrets["PA1M23"],st.secrets["PA1M24"]]
+            
+            url_list2 = ["PA2M04", "PA2M05", "PA2M06", "PA2M07", "PA2M08", "PA2M09", "PA2M10", "PA2M11", "PA2M12", "PA2M13", "PA2M14", "PA2M15", "PA2M15", "PA2M17", "PA2M18", "PA2M19", "PA2M20", "PA2M21", "PA2M22", "PA2M23", "PA2M24"]
+            
+            st.text("Modalidad: {}, Asignatura: {}, Modulo: {}".format(op_modalidad,op_asignatura,op_modulo))
+
+            df = generarDF(url_list2)
+
+            if op_modulo == "Todos":
+                st.write(df)
+            else:
+                try:
+                    st.write(df[df["Módulo"] == int(op_modulo)])
+                except:
+                    st.write(df[df["Módulo"] == op_modulo])
+            
         case "A3":
             op_modulo = st.selectbox("Elegir Módulo", ["Todos","2","3","4","5"])
-            url_list = [st.secrets["PA3M02"],st.secrets["PA3M03"],st.secrets["PA3M04"],st.secrets["PA3M05"]]
+            
+            url_list2 = ["PA3M02", "PA3M03", "PA3M04", "PA3M05"]
+            
+            st.text("Modalidad: {}, Asignatura: {}, Modulo: {}".format(op_modalidad,op_asignatura,op_modulo))
+
+            df = generarDF(url_list2)
+
+            if op_modulo == "Todos":
+                st.write(df)
+            else:
+                try:
+                    st.write(df[df["Módulo"] == int(op_modulo)])
+                except:
+                    st.write(df[df["Módulo"] == op_modulo])
+                    
         case "A4":
-            op_modulo = st.selectbox("Elegir Módulo", ["Todos","02","03","04","05","06","07","08","09","10","11","12","0x"])
+            op_modulo = st.selectbox("Elegir Módulo", ["Todos","2","3","4","5","6","7","8","9","10","11","12","x"])
 
             url_list2 = ["PA4M02", "PA4M03", "PA4M04", "PA4M05", "PA4M06", "PA4M07", "PA4M08", "PA4M09", "PA4M10", "PA4M11", "PA4M12", "PA4M0x"]
             
@@ -104,16 +190,14 @@ else: #Práctico
 
             df = generarDF(url_list2)
 
-
             if op_modulo == "Todos":
                 st.write(df)
             else:
-                st.write(df[df["Módulo"] == op_modulo])
+                try:
+                    st.write(df[df["Módulo"] == int(op_modulo)])
+                except:
+                    st.write(df[df["Módulo"] == op_modulo])
 
-                
-            
-
-              
                     
 st.text("Modalidad: {}, Asignatura: {}, Modulo: {}".format(op_modalidad,op_asignatura,op_modulo))
 
